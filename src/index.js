31f5fa085f5014e56f020193b34f9fe982425522
pablo <pablo.gadino@gmail.com>
2021-07-25 20:52:42 -0300

    putos

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { getUsers } from "./users";
 export async function printUsers() {
   const users = await getUsers();
   const element = document.createElement("div");
-  element.innerHTML = `<h2>Current users</h2>
+  element.innerHTML = `<h2>Current putos users</h2>
     ${users.map((user) => `<div>${user.name}</div>`).join("")}`;
   return element;
 }
